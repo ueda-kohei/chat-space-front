259224bb98febd444e58fe26c2eb505b065a379a
ueda-kohei <kouidgtr@gmail.com>
2019-11-11 21:04:05 +0900

    chatspace_front

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,13 @@ class MessagesController < ApplicationController
 def index
 end
 
+def new
+  @messages = Messages.new
+end
+
+def create
+  @messages = Messages.new(params[:messages])
+  @messages.save
+end
+
 end
